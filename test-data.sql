e1f781c9b5a0fe8770df63a12e2833bd0bca6a75
“[Ryan <[ryanmiller12@bellsouth.net]>
2018-06-22 10:13:24 -0400

    created queries and test data

---
+++ test-data.sql
@@ -0,0 +1,6 @@
+insert into TodoList (title, isDone)
+
+values('Smack Chris Aquino with a pie to the face for asterisk exercises.', false),
+      ('Wash car.', true),
+      ('Create newsletter for personal site.', false),
+      ('Finish creating unfinished song projects.', false);
